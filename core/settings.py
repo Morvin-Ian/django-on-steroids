223e21acc4b02b60a5894a9cd38e21e81c2e5590
Morvin-Ian <morvinian@gmail.com>
2023-04-16 00:10:17 +0300

    ignore

--- core/settings.py
+++ core/settings.py
@@ -15,15 +15,15 @@ import os
 import environ
 
 
-env = environ.Env()
-
-# read th .env file
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+
+# read th .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
