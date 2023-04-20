304f8f135d0c9e48af48ecf4369d39fb108d8ef7
Morvin-Ian <morvinian@gmail.com>
2023-04-20 16:39:21 +0300

    messaging tests

--- messaging/tests/test_models.py
+++ messaging/tests/test_models.py
@@ -8,6 +8,4 @@ class TestMessageModel(APITestCase):
     Tests for the Message Model
     """
 
-    def test_str(self):
-        message = Message.objects.create(1,2,'Hello There')
-        self.assertEqual(str(message),'Hello There')
+ 
