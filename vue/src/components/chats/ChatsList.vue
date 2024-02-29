a0619fc3ee1a046626b4a8b088873384c7864a5a
Morvin-Ian <morvinian@gmail.com>
2024-03-01 00:30:07 +0300

    clean up

---
+++ vue/src/components/chats/ChatsList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="chat-list">
+        <div class="chat" 
+            v-for="chat in chatStore.sortedChats" 
+            :key="chat.id">
+                <Chat @click="setActiveChat(chat.id)" :chat="chat"/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import {defineEmits} from "vue"
+    import Chat from "@/components/chats/Chat.vue";
+    import {useChatStore} from "@/stores/chats.js"
+    import {useActiveChatStore} from "@/stores/activeChat.js"
+
+    const chatStore = useChatStore();
+    const activeChatStore = useActiveChatStore();
+
+    const emits = defineEmits(['change-view'])
+    //fetch chats
+    chatStore.getChats()
+
+    const setActiveChat = (id) =>{
+        activeChatStore.setChat(id)
+        emits('change-view', false)
+    }
+
+
+    
+</script>
+
+<style scoped>
+    .chat-list {
+        margin-top: 5px; 
+        height: 85vh;
+        overflow-y: scroll;       
+    }
+
+</style>
