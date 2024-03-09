0f6a449afc96d1f376cf5f7ed3cb2b14d5bafd52
Morvin-Ian <morvinian@gmail.com>
2024-03-10 02:42:43 +0300

    changes

--- frontend/src/components/messages/MessagesList.vue
+++ frontend/src/components/messages/MessagesList.vue
@@ -10,13 +10,34 @@
 <script setup>
 import Message from "@/components/messages/Message.vue"
 import { useMessagesStore } from "@/stores/messages.js"
+import { useChatStore } from "@/stores/chats";
+import { useSocketStore } from "@/stores/socket";
+import { onMounted, watch } from "vue";
 
 const messageStore = useMessagesStore()
+const socketStore = useSocketStore()
+const chatStore = useChatStore()
 const user = JSON.parse(localStorage.getItem("user"))
 
 // fetch Messages
 messageStore.fetchMessages(user.token)
 
+watch(() => socketStore.socket, (newSocket, oldSocket) => {
+    if (newSocket) {
+        newSocket.onmessage = () => {
+            chatStore.getChats(user.token);
+            messageStore.fetchMessages(user.token);
+        };
+
+    }
+});
+
+onMounted(()=>{
+    socketStore.socket.onmessage = () => {
+        chatStore.getChats(user.token)
+        messageStore.fetchMessages(user.token)
+    }
+})
 </script>
 
 <style scoped>
