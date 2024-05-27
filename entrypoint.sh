f35856c9140f142fe5f884133060017fab470085
Morvin-Ian <morvinian@gmail.com>
2024-05-27 21:48:28 +0300

    minor rename

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-gunicorn core.wsgi --bind 0.0.0.0:8000
+gunicorn core.wsgi:aplication --bind 0.0.0.0:8000 --workers 4
